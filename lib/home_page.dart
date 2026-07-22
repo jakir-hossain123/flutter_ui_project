4a24d9888b4ba60f9b1c8518a72e416fdb010974
Md.Jakir Hossain <jakir7z7z7z7z@gmail.com>
2026-07-22 13:08:01 +0600

    Profile page ui completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -8,7 +10,7 @@ class HomePage extends StatelessWidget {
     return Scaffold(
          appBar: AppBar(
            backgroundColor: Colors.amber,
-           title: const Text('My Profile',style: TextStyle(fontWeight: FontWeight.bold),),
+           title: const Text('My Profile',style: TextStyle(fontWeight: FontWeight.w400),),
            actions: [
              Icon(Icons.add),
              SizedBox(width: 12,),
@@ -19,6 +21,37 @@ class HomePage extends StatelessWidget {
 
            ],
          ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              SizedBox(height: 8,),
+              Container(
+                height: 120,
+                width: 120,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.deepPurple[100],
+                ),
+                child: Icon(Icons.icecream_outlined,size: 80,color: Colors.deepPurple.shade900,),
+              ),
+              SizedBox(height: 10,),
+              Text("Ice cream is very delicious right?",style: TextStyle(fontWeight: FontWeight.bold),),
+              const SizedBox(height: 30,),
+              Container(
+                height: 120,
+                width: 120,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.deepPurple[100],
+                ),
+                child: Icon(Icons.code,size: 80,color: Colors.deepPurple.shade900,),
+              ),
+              SizedBox(height: 10,),
+              Text("programming is not boring if you love it",style: TextStyle(fontWeight: FontWeight.bold),),
+            ],
+          ),
+        ),
     );
   }
 }
