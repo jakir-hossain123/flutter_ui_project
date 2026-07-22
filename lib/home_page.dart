c27df3d91a5cc8868febdebf7e9eb2ed8c2e31f6
Md.Jakir Hossain <jakir7z7z7z7z@gmail.com>
2026-07-22 12:23:14 +0600

    appbar done

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -6,7 +6,19 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+         appBar: AppBar(
+           backgroundColor: Colors.amber,
+           title: const Text('My Profile',style: TextStyle(fontWeight: FontWeight.bold),),
+           actions: [
+             Icon(Icons.add),
+             SizedBox(width: 12,),
+             Icon(Icons.settings),
+             SizedBox(width: 12,),
+             Icon(Icons.call),
+             SizedBox(width: 5,),
 
+           ],
+         ),
     );
   }
 }
